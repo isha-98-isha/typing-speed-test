dc4a81333ba108def80cd97f69ff130c840b3844
Isha-98-isha <ishachaudhari98@gmail.com>
2025-08-05 16:50:36 +0530

    unsplash

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,11 @@
 // src/app/app.ts
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html'
+  imports: [RouterOutlet],
+  template: `<router-outlet />`
 })
-export class App {
-  showSplash = true;
-
-  ngOnInit() {
-    setTimeout(() => {
-      this.showSplash = false;
-    }, 5000); // Show splash for 5 seconds
-  }
- }
+export class App { }
